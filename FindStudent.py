ecc407b0affc46a931a3d1c224e06f4a9f065fb4
Mr.Guo <76673730+NepenthesJH@users.noreply.github.com>
2023-04-10 09:12:46 +0800

    Add files via upload

--- FindStudent.py
+++ FindStudent.py
@@ -4,7 +4,8 @@ import os
 
 # 读取班级名单
 students = pd.read_csv('Students.csv')
-students = np.array(students)
+students = pd.DataFrame(students)
+
 # 读取作业文件夹，实际使用把作业文件夹放到这个项目里面，修改文件夹名字即可
 file_dir = "./测试作业"
 for root, dirs, homework in os.walk(file_dir, topdown=False):
@@ -15,20 +16,22 @@ for root, dirs, homework in os.walk(file_dir, topdown=False):
 total_number = len(students)
 have_number = len(homework)
 temp_mat = np.zeros(total_number)
+
 for i in range(total_number):
     tmp = -1
     for j in range(have_number):
-        if homework[j].find(students[i][1]) != -1:
+        if homework[j].find(students["姓名"][i]) != -1:
             tmp = 0
             break
     temp_mat[i] = tmp
 
 no_number = np.count_nonzero(temp_mat)
 
+# 这里的have_index、no_index是个元组，调用需要加入下标
 have_index = np.where(temp_mat == 0)
 no_index = np.where(temp_mat == -1)
 
-print("已经交作业的人数：", have_number)
-print("已经交作业的人：\n", students[have_index].T)
+print("\n已经交作业的人数：", have_number)
+print("已经交作业的人：\n", students["姓名"][have_index[0]])
 print("未交作业的人数：", no_number)
-print("未交作业的人：\n", students[no_index].T)
+print("未交作业的人：\n", students["姓名"][no_index[0]])
