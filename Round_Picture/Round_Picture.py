d58ed031314adec298b223c5c69b935a4f9143c4
Mr.Nepenthes <Nepenthes.J.H@gmail.com>
2023-09-17 23:21:15 +0800

    README.md

--- Round_Picture/Round_Picture.py
+++ Round_Picture/Round_Picture.py
@@ -12,7 +12,7 @@ def matrix_to_image(matrix, output_path):
     image.save(output_path)
 
 if __name__ == "__main__":
-    image_path = "直角图片.jpg"  # 替换成实际的图像文件路径
+    image_path = "直角照片.jpg"  # 替换成实际的图像文件路径
     image_matrix = image_to_matrix(image_path)
     height, weight, channels = image_matrix.shape  # 图片尺寸
     n = 60  # 圆角化的半径
@@ -31,6 +31,6 @@ if __name__ == "__main__":
             if (n - height + i)**2 + (n - weight + j)**2 > n**2 and i > height - n and j > weight - n:
                 image_matrix[i][j] = [255, 255, 255]
 
-    output_path = '圆角照片.png'
+    output_path = '圆角照片.jpg'
     matrix_to_image(image_matrix, output_path)
     print("照片圆角化成功！！！")
